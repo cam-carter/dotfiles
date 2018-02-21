33d27f0061fc8287fe630ea91c54b4ced94fc724
Cam <ccprimitivo@gmail.com>
2018-02-21 12:17:32 -0600

    Added some old aliases from .bashrc

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -1,7 +1,16 @@
-#############################
-#### ALIASES & FUNCTIONS ####
-#############################
+#################
+#### ALIASES ####
+#################
 
 alias v="vim"
 alias cl="clear"
 alias update="apt update && apt upgrade; apt autoremove"
+alias djangodev='source ~/.virtualenvs/djangodev/bin/activate'
+alias macspoof='bash ~/.bin/macspoof'
+alias pc='proxychains'
+alias spotify='/usr/bin/spotify --force-device-scale-factor=1.5'
+alias loadrvm='source /usr/local/rvm/bin/rvm'
+alias startapache='/etc/init.d/apache2 start'
+alias stopapache='/etc/init.d/apache2 stop'
+alias startmysql='/etc/init.d/mysql start'
+alias stopmysql='/etc/init.d/mysql stop'
