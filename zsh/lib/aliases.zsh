8c98c8579037dd8060becda95eb3d746ef6c10cb
Cam <ccprimitivo@gmail.com>
2018-02-21 11:19:30 -0600

    Added separate functions.zsh for zshrc

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -2,52 +2,7 @@
 #### ALIASES & FUNCTIONS ####
 #############################
 
-c() {
-	cd $1;
-	ls;
-}
 alias cd="c"
 alias v="vim"
 alias cl="clear"
-
-# parent_up
-# this function jumps to the parent directory and binds it to "^k"
-function parent_up() {
-	BUFFER="cd .."
-	zle accept-line
-}
-zle -N parent_up
-bindkey "^k" parent_up
-
-# goto home
-# this function jumps to home directory and binds it to "^h"
-function goto_home() {
-	BUFFER="cd ~"$BUFFER
-	zle end-of-line
-	zle accept-line
-}
-zle -N goto_home
-bindkey "^h" goto_home
-
-# quick list
-# this function lists the current directory and binds it to "^l"
-function quick_ls() {
-	BUFFER="ls"
-	zle accept-line
-}
-zle -N quick_ls
-bindkey "^l" quick_ls
-
-# git stage
-# this function adds all changes in the repo, commits them, and then pushes the repo
-function git_stage() {
-	if [ -n "$BUFFER" ]; then
-		BUFFER="git add -A; git commit -m \"$BUFFER\" && git push"
-	elif [ -z "$BUFFER" ]; then
-	 	BUFFER="git add -A; git commit -v && git push"
-   	fi	
-
-	zle accept-line
-}
-zle -N git_stage
-bindkey "^g" git_stage
+alias update="apt update && apt upgrade; apt autoremove"
